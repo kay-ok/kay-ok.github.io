bb07080ea72fad8cd8ef50908da86ea264da86b0
kay-ok <okubokei870916@gmail.com>
2023-08-15 13:35:01 +0900

    ローディングアニメーションの速度変更

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ var stroke;
 stroke = new Vivus('mask', {//アニメーションをするIDの指定
     start:'manual',//自動再生をせずスタートをマニュアルに
     type: 'scenario-sync',// アニメーションのタイプを設定
-    duration: 6.3,//アニメーションの時間設定。数字が小さくなるほど速い
+    duration: 7.3,//アニメーションの時間設定。数字が小さくなるほど速い
     forceRender: false,//パスが更新された場合に再レンダリングさせない
     animTimingFunction:Vivus.linear,//動きの加速減速設定
 },
@@ -142,8 +142,8 @@ function(){
 );
 
 $(window).on('load',function(){
-  $("#splash").delay(2400).fadeOut(200);//ローディング画面を3秒（3000ms）待機してからフェイドアウト
-	$("#splash_logo").delay(2400).fadeOut(200);//ロゴを3秒（3000ms）待機してからフェイドアウト
+  $("#splash").delay(2400).fadeOut(0);//ローディング画面を3秒（3000ms）待機してからフェイドアウト
+	$("#splash_logo").delay(2400).fadeOut(0);//ロゴを3秒（3000ms）待機してからフェイドアウト
         stroke.play();//SVGアニメーションの実行
 });
 
