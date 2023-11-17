efef3be995b481d0ca524769360220e345e27e25
kay-ok <okubokei870916@gmail.com>
2023-11-17 13:33:56 +0900

    fixed some codes in js file

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -59,15 +59,15 @@ var ctx1 = document.getElementById("myRadarChart");
       responsive: true,
       scales: {
         r: {
-          max: 10,        //グラフの最大値
-          min: 0,        //グラフの最小値
+          max: 10,
+          min: 0,
           pointLabels: {
             font: {
               size: 10.5
             }
           },
           ticks: {
-            stepSize: 1,  //目盛間隔
+            stepSize: 1,
           },
           angleLines: {
             color: 'green',
@@ -86,11 +86,11 @@ var ctx1 = document.getElementById("myRadarChart");
 
 
 // 横棒グラフ
-$('#chart').on('inview', function(event, isInView) {//画面上に入ったらグラフを描画
+$('#chart').on('inview', function(event, isInView) {
   if (isInView) {
     var ctx = document.getElementById('chart');
     var data = {
-      labels: ["HTML", "CSS", "JavaScript", "jQuery", "React"],
+      labels: ["HTML", "CSS", "JavaScript", "jQuery", "Other"],
       datasets: [{
           label: '現役エンジニアを10とした自身のスキル',
           data: [8, 7, 4, 7, 3],
@@ -140,86 +140,57 @@ var chart = new Chart(ctx, {
 // スキルバーここまで
 
 
-// ウェーブ最初の色設定
-// colorList.push(['#0ff', '#ff0', '#fff', '#00f', '#f0f']);
-
-
-
-
-// ウェーブここから
-
-
-
-// 波線ここまで
-
-
-// 波線複数ここから
-
 var unit = 100,
-    canvasList, // キャンバスの配列
-    info = {}, // 全キャンバス共通の描画情報
-    colorList; // 各キャンバスの色情報
+    canvasList,
+    info = {},
+    colorList;
 
-/**
- * Init function.
- * 
- * Initialize variables and begin the animation.
- */
+
 function init() {
     info.seconds = 0;
     info.t = 0;
     canvasList = [];
     colorList = [];
-    // canvas1個めの色指定
+
     canvasList.push(document.getElementById("waveCanvas"));
-    colorList.push(['#0ff', '#ff0', '#fff', '#00f', '#ff99cc']);//重ねる波線の色設定
+    colorList.push(['#0ff', '#ff0', '#fff', '#00f', '#ff99cc']);
     
-    // canvas2個めの色指定
+
     canvasList.push(document.getElementById("waveCanvas2"));
     colorList.push(['#43c0e4']);
     
-    // canvas3個めの色指定
+
     canvasList.push(document.getElementById("waveCanvas3"));
     colorList.push(['#cc99ff']);
 
   
-    // 各キャンバスの初期化
     for(var canvasIndex in canvasList) {
         var canvas = canvasList[canvasIndex];
         canvas.width = document.documentElement.clientWidth; //Canvasのwidthをウィンドウの幅に合わせる
         canvas.height = 200;//波の高さ
         canvas.contextCache = canvas.getContext("2d");
     }
-    // 共通の更新処理呼び出し
+
     update();
 }
 
 function update() {
     for(var canvasIndex in canvasList) {
         var canvas = canvasList[canvasIndex];
-        // 各キャンバスの描画
         draw(canvas, colorList[canvasIndex]);
     }
-    // 共通の描画情報の更新
+
     info.seconds = info.seconds + .014;
     info.t = info.seconds*Math.PI;
-    // 自身の再起呼び出し
+
     setTimeout(update, 35);
 }
 
-/**
- * Draw animation function.
- * 
- * This function draws one frame of the animation, waits 20ms, and then calls
- * itself again.
- */
+
 function draw(canvas, color) {
-    // 対象のcanvasのコンテキストを取得
     var context = canvas.contextCache;
-    // キャンバスの描画をクリア
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    //波を描画 drawWave(canvas, color[数字（波の数を0から数えて指定）], 透過, 波の幅のzoom,波の開始位置の遅れ )
     drawWave(canvas, color[0], 0.8, 3, 0);
     drawWave(canvas, color[1], 0.5, 4, 0);
     drawWave(canvas, color[2], 0.3, 1.6, 0);
@@ -227,37 +198,27 @@ function draw(canvas, color) {
     drawWave(canvas, color[4], 0.8, 2, 0);
 }
 
-/**
-* 波を描画
-* drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
-*/
+
 function drawWave(canvas, color, alpha, zoom, delay) {
   var context = canvas.contextCache;
-    context.strokeStyle = color;//線の色
-  context.lineWidth = 1;//線の幅
+    context.strokeStyle = color;
+  context.lineWidth = 1;
     context.globalAlpha = alpha;
-    context.beginPath(); //パスの開始
+    context.beginPath();
     drawSine(canvas, info.t / 0.5, zoom, delay);
-    context.stroke(); //線
+    context.stroke();
 }
 
-/**
- * Function to draw sine
- * 
- * The sine curve is drawn in 10px segments starting at the origin. 
- * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
- */
+
 function drawSine(canvas, t, zoom, delay) {
     var xAxis = Math.floor(canvas.height/2);
     var yAxis = 0;
     var context = canvas.contextCache;
-    // Set the initial x and y, starting at 0,0 and translating to the origin on
-    // the canvas.
-    var x = t; //時間を横の位置とする
+    var x = t;
     var y = Math.sin(x)/zoom;
-    context.moveTo(yAxis, unit*y+xAxis); //スタート位置にパスを置く
+    context.moveTo(yAxis, unit*y+xAxis);
 
-    // Loop to draw segments (横幅の分、波を描画)
+
     for (i = yAxis; i <= canvas.width + 10; i += 10) {
         x = t+(-yAxis+i)/unit/zoom;
         y = Math.sin(x - delay)/3;
